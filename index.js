ac512bd529e2845502dae8f87e60c74d356e4440
haizero191 <44278262+haizero191@users.noreply.github.com>
2020-04-27 07:30:40 +0700

    add file index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var port = 3000;
 
 app.get("/", function(request, response) {
-	response.send("<h1>con cac dit me may</h1>");
+	response.send("<h1>Hello may cung</h1>");
 })
 app.listen(port, function(){
 	console.log('server listening on port' + port);
